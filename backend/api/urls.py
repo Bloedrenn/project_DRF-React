2be1bd81644dfccb3c0a136a6069c02ff38419e1
Bloedrenn <bloedrenn@gmail.com>
2025-04-19 21:24:21 +0300

    Первые шаги в DRF

---
+++ backend/api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from rest_framework import routers
+from .views import ItemViewSet
+
+router = routers.DefaultRouter()
+router.register(r'items', ItemViewSet)  # Авто-генерация URL для ViewSet
+
+urlpatterns = [
+    path('', include(router.urls)),  # Все маршруты роутера
+]
