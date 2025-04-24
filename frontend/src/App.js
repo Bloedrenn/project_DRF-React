21cddad4654b62a0d19555d8ae72e4ffe8215764
Bloedrenn <bloedrenn@gmail.com>
2025-04-25 02:46:34 +0300

    React: переместил Header.js в папку components/

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
-import Header from './Header';
+import Header from './components/Header';
 
 class App extends Component {
     constructor(props) {
