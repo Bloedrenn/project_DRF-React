3fd470516e617a09e1ce0b9934d4ee46c6ddff37
Bloedrenn <bloedrenn@gmail.com>
2025-04-25 02:24:24 +0300

    React: превратил функц. компонент Header в классовый

--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -1,7 +1,11 @@
-function Header() {
-    return (
-        <header>Шапка сайта</header>
-    );
+import React from 'react';
+
+class Header extends React.Component {
+    render() {
+        return (
+            <header>Шапка сайта</header>
+        )
+    }
 }
 
 export default Header;
