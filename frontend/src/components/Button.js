126aa5fffba98118eb82476da89f343da2fd12b8
Bloedrenn <bloedrenn@gmail.com>
2025-04-25 17:31:53 +0300

    React: хук useEffect в функ. компоненте

--- frontend/src/components/Button.js
+++ frontend/src/components/Button.js
@@ -1,7 +1,11 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Button = ({ text="Кнопка" }) => {
     const [click, setClick] = useState(0)
+
+    useEffect(() => {
+        document.title = `Вы нажали ${click} раз`
+    })
 
     return <button onClick={() => {setClick(click + 1)}}>{text} Клик: {click}</button>;
 };
