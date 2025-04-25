293b85831f50afd30ee28e5a04da1cc5cd4d522e
Bloedrenn <bloedrenn@gmail.com>
2025-04-25 17:17:49 +0300

    React: изменил состояние в функ. компоненте (useState)

--- frontend/src/components/Button.js
+++ frontend/src/components/Button.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Button = ({ text="Кнопка" }) => {
-    return <button>{text}</button>;
+    const [click, setClick] = useState(0)
+
+    return <button onClick={() => {setClick(click + 1)}}>{text} Клик: {click}</button>;
 };
 
 export default Button;
