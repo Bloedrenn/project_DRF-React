30918dec92e7ed97dfcfa2e3779089a210ec6509
Bloedrenn <bloedrenn@gmail.com>
2025-04-28 01:12:29 +0300

    React: добавление users - сохранил данные из input'ов в state

--- frontend/src/components/AddUser.js
+++ frontend/src/components/AddUser.js
@@ -1,16 +1,28 @@
 import React from 'react';
 
 class AddUser extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            firstname: "",
+            lastname: "",
+            bio: "",
+            age: 1,
+            isHappy: false
+        }
+    }
+
     render() {
         return (
             <form>
-                <input placeholder='Имя' />
-                <input placeholder='Фамилия' />
-                <textarea placeholder='Биография'></textarea>
-                <input placeholder='Возраст' />
+                <input placeholder='Имя' onChange={(e) => this.setState({ firstname: e.target.value })} />
+                <input placeholder='Фамилия' onChange={(e) => this.setState({ lastname: e.target.value })} />
+                <textarea placeholder='Биография' onChange={(e) => this.setState({ bio: e.target.value })}></textarea>
+                <input placeholder='Возраст' onChange={(e) => this.setState({ age: e.target.value })} />
 
                 <label htmlFor='isHappy'>Счатлив?</label>
-                <input type='checkbox' id='isHappy' />
+                <input type='checkbox' id='isHappy' onChange={(e) => this.setState({ isHappy: e.target.checked })} />
 
                 <button type='button'>Добавить</button>
             </form>
