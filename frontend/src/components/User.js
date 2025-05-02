919239921e62e2ad4d1d3301c89c41701e7cc437
Bloedrenn <bloedrenn@gmail.com>
2025-05-02 21:51:11 +0300

    React: функционал редактирования пользователей - добавил форму

--- frontend/src/components/User.js
+++ frontend/src/components/User.js
@@ -2,17 +2,29 @@ import React from 'react';
 import { IoCloseCircleSharp } from 'react-icons/io5';
 import { FaEdit } from "react-icons/fa";
 
+import AddUser from "./AddUser";
+
 class User extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            editForm: false
+        }
+    }
+
     user = this.props.user;
 
     render() {
         return (
             <div className="user">
                 <IoCloseCircleSharp onClick={() => this.props.onDelete(this.user.id)} className='delete-icon' />
-                <FaEdit className='edit-icon' />
+                <FaEdit onClick={() => this.setState({editForm: !this.state.editForm}) } className='edit-icon' />
                 <h3>{this.user.firstname} {this.user.lastname}</h3>
                 <p>{this.user.bio}</p>
                 <b>{this.user.isHappy ? 'Счастлив :)' : 'Не особо :('}</b>
+
+                {this.state.editForm && <AddUser />}
             </div>
         )
     }
