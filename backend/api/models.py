c3b7a8cb28525a406f73ec985ecf29de909cd6f0
Bloedrenn <bloedrenn@gmail.com>
2025-05-06 15:59:47 +0300

    Django: добавил к полю description null=True

--- backend/api/models.py
+++ backend/api/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Item(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField(blank=True)
+    description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     is_available = models.BooleanField()
 
