3ecc08337f15c429e8e5acb590aa168029044e0f
Bloedrenn <bloedrenn@gmail.com>
2025-05-07 19:47:17 +0300

    React: улучшил структуру проекта - переименовал папку Items в ItemComponent

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,5 +1,5 @@
-import Items from '../components/Items/ItemList/Items';
-import AddItem from '../components/Items/AddItemForm/AddItem';
+import Items from '../components/ItemComponent/ItemList/Items';
+import AddItem from '../components/ItemComponent/AddItemForm/AddItem';
 
 const HomePage = ({ items, onAdd, onEdit, onDelete }) => (
     <>
