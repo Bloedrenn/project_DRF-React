92a1691d97a14b0971c747774118a3ad307c88ac
Bloedrenn <bloedrenn@gmail.com>
2025-05-07 20:15:05 +0300

    React: улучшил структуру проекта - (исправление предыдущего коммита, в посл. раз) исправил импорт Items на ItemList

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,10 +1,10 @@
-import Items from '../components/ItemComponent/ItemList';
+import ItemList from '../components/ItemComponent/ItemList';
 import AddItem from '../components/ItemComponent/AddItemForm/AddItem';
 
 const HomePage = ({ items, onAdd, onEdit, onDelete }) => (
     <>
         <main>
-            <Items items={items} onEdit={onEdit} onDelete={onDelete} />
+            <ItemList items={items} onEdit={onEdit} onDelete={onDelete} />
         </main>
         <aside>
             <AddItem onAdd={onAdd} />
