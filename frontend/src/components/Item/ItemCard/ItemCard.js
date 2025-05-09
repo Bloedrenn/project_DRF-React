b83efeea78e731106dcbe9da679ccf1364c47dcb
Bloedrenn <bloedrenn@gmail.com>
2025-05-09 18:40:02 +0300

    React: изучение Redux ч. 3 - реализовал второй редюсер (customersReducer)

--- frontend/src/components/Item/ItemCard/ItemCard.js
+++ frontend/src/components/Item/ItemCard/ItemCard.js
@@ -5,6 +5,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import { IoCloseCircleSharp } from 'react-icons/io5';
 import { FaEdit } from "react-icons/fa";
 
+import { addCustomerAction, removeCustomerAction } from '../../../store/reducers/customersReducer';
+
 import EditItem from "../Forms/EditItem";
 
 import styles from './ItemCard.module.css';
@@ -14,6 +16,7 @@ const ItemCard = (props) => {
 
     const dispatch = useDispatch();
     const cash = useSelector(state => state.cash.cash);
+    const customers = useSelector(state => state.customers.customers)
 
     const addCash = (amount) => {
         dispatch({ type: "ADD_CASH", payload: amount });
@@ -21,6 +24,15 @@ const ItemCard = (props) => {
 
     const getCash = (amount) => {
         dispatch({ type: "GET_CASH", payload: amount });
+    }
+
+    const addCustomer = (customerName) => {
+        const customer = { id: customers.length + 1, name: customerName } // Можно использовать Date.now() в id
+        dispatch(addCustomerAction(customer))
+    }
+
+    const removeCustomer = (id) => {
+        dispatch(removeCustomerAction(id))
     }
 
     return (
@@ -45,6 +57,18 @@ const ItemCard = (props) => {
             <button onClick={() => addCash(Number(prompt()))}>Пополнить счёт</button>
             <button onClick={() => getCash(Number(prompt()))}>Снять со счёта</button>
             <div>{cash}</div>
+
+            <button onClick={() => addCustomer(prompt())}>Добавить клиента</button>
+            {
+                customers.length > 0 ?
+                <div>
+                    {customers.map(customer => 
+                        <div onClick={() => removeCustomer(customer.id)} key={customer.id}>{customer.name}</div>
+                    )}
+                </div>
+                :
+                <div>Клиенты отсутствуют</div>
+            }
 
             {editForm && <EditItem onEdit={props.onEdit} item={props.item} />}
         </div>
